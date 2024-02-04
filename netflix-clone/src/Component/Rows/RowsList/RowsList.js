7c3ab4e470b8a5bebc52b0a0abca889a02fea27a
getahunabr <147873183+getahunabr@users.noreply.github.com>
2024-02-03 21:39:08 -0800

    deploy

---
+++ netflix-clone/src/Component/Rows/RowsList/RowsList.js
@@ -0,0 +1,25 @@
+// import "./row.css";
+import Row from "../Row";
+import request from "../../../utils/request";
+
+const RowsList = () => {
+  return (
+    <div>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={request.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+      <Row title="Top rated" fetchUrl={request.fetchTopRatedMovies} />
+      <Row title="Action movie" fetchUrl={request.fetchActionMovies} />
+      <Row title="comedy movie" fetchUrl={request.fetchComedyMovies} />
+      <Row title="Horror movie" fetchUrl={request.fetchHorrorMovies} />
+      <Row title="Romance movie" fetchUrl={request.fetchRomanceMovies} />
+      <Row title="TvShow" fetchUrl={request.fetchTvShow} />
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
+    </div>
+  );
+};
+
+export default RowsList;
--- netflix-clone/src/Pages/Home.js
+++ netflix-clone/src/Pages/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../Component/Header/Header";
 import Footer from "../Component/Footer/Footer";
 import Banner from "../Component/Banner/Banner";
+import RowsList from "../Component/Rows/RowsList/RowsList";
 
 const Home = () => {
   return (
     <>
       <Header />
       <Banner />
+      <RowsList />
       <Footer />
     </>
   );
